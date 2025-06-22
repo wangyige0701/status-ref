8d143bef2b2d4fe8bfe82091138783628fab4c71
WangYige <1776686515@qq.com>
2025-06-22 15:56:34 +0800

    vue 处理优化

--- src/vue/index.ts
+++ src/vue/index.ts
@@ -1,17 +1,46 @@
-import { track, trigger, TrackOpTypes, TriggerOpTypes } from '@vue/reactivity';
+import { customRef } from 'vue';
+import { VOID_FUNCTION, type Fn } from '@wang-yige/utils';
 import { type StatusRefResult, StatusRef } from '@/index';
 
 export type { StatusRefResult };
 
 export { StatusRef };
 
-export const useVueStatusRef = StatusRef.create(() => {
+/**
+ * @example
+ * ```vue
+ * <template>
+ * 	<div v-if="statusRef.loading">Loading...</div>
+ * 	<div v-else-if="statusRef.success">Success!</div>
+ * 	<div v-else>Error!</div>
+ * </template>
+ * <script setup lang="ts">
+ * import { useVueStatusRef } from 'StatusRef/vue';
+ *
+ * const status = useVueStatusRef('loading', ['success', true] as const);
+ * status.loading // false
+ * status.success // true
+ *
+ * status.toggleLoading();
+ * </script>
+ * ```
+ */
+export const useVueStatusRef = StatusRef.create((_, initial) => {
+	let track: Fn;
+	let trigger: Fn;
+	let bool: boolean = initial;
+	customRef(($1, $2) => {
+		(track = $1), (trigger = $2);
+		return { get: VOID_FUNCTION, set: VOID_FUNCTION };
+	});
 	return {
-		track: (target, key) => {
-			track(target, TrackOpTypes.GET, key);
+		track: () => {
+			track();
+			return bool;
 		},
-		trigger: (target, key, status) => {
-			trigger(target, TriggerOpTypes.SET, key, status);
+		trigger: (_$1, _$2, status) => {
+			bool = status;
+			trigger();
 		},
 	};
 });
